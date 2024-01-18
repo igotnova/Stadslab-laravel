0e48a7f0ba9d624167c921b5fbda75f2d11a4646
igotnova <onnoprins97@gmail.com>
2024-01-18 14:47:57 +0100

    setup laravel

---
+++ resources/views/post.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stadslab posts</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+    <?= $post;?>
+    </article>
+    <h1><a href="/">go back</a></h1>
+</body>
+</html>
